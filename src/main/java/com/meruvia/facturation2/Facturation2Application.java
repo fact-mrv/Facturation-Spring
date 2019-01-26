a63df0225c1810aa0b4768416f3400dcb39a765a
meruvi <gmeruviarojas@gmail.com>
2019-01-26 13:51:02 -0600

    ORIGIN

---
+++ src/main/java/com/meruvia/facturation2/Facturation2Application.java
@@ -0,0 +1,14 @@
+package com.meruvia.facturation2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Facturation2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Facturation2Application.class, args);
+	}
+
+}
+
